e681ccc5faea27c1668571fb9df846a40a15cffb
tattk <152263071+tattk@users.noreply.github.com>
2023-12-17 14:47:44 +0900

    Add files via upload

---
+++ python_scraping_tutorial.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Jupyter Notebookで作成し，一部デフォルメ
+[[https://note.nkmk.me/python/|note.nkmk.me site]]
+
+#和暦西暦htmlの表をcsvで一括取得
+#データ分析lib pandasでtable取得
+import pandas as pd
+
+url = 'https://www.jcb.co.jp/processing/share/wareki.html'
+dfs = pd.read_html(url)
+print(len(dfs))
+
+#head()メソッドのデフォは先頭5行
+print(dfs[0].head())
+
+#DataFrameの連結;concat, ignore_indexで再連番取得
+df = pd.concat([dfs[0],dfs[1],dfs[2],dfs[3],dfs[4]], ignore_index=True)
+print(df)
+#和暦列をindexにする
+df_reset = df.set_index('和暦')
+print(df_reset)
+
+#wareki.csvファイルとして出力
+df_reset.to_csv('/Users/username/Desktop/wareki.csv', mode='w')
+
+
---
+++ write_and_add.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Jupyter Notebookで作成し，一部デフォルメ
+[[https://note.nkmk.me/python/|note.nkmk.me site]]
+
+#Pythonで任意の.txtへ書き込みを行うコード
+#encoding確認
+import locale
+print(locale.getpreferredencoding())
+
+#実行前にターミナルのパス権限確認
+path = '/Users/username/Desktop/filename.txt'
+with open(path) as f:
+    print(type(f))
+
+#filename.txtへ書き込み
+#mode='w'は上書き、'a'は末尾に追記.どちらもファイルなければ新規作成
+
+s = '\nadd line' 
+with open(path, mode='a') as f:
+    f.write(s)
+with open(path) as f:
+    print(f.read())
+
+#配列、
+l = ['x', 'y', 'z']
+l.insert(0,'First')
+
+with open(path,mode='w') as f:
+    f.write('\n'.join(l))
+
+with open(path) as f:
+    print(f.read())
+
